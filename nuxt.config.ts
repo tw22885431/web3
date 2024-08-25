de384a407d59b05d846f7e5c215a8e66863676aa
Ben Wu <re22885431@gmail.com>
2024-08-25 20:44:03 +0800

    add firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+    modules: ['nuxt-vuefire'],
+   	vuefire: {
+        // ensures the auth module is enabled
+        auth: {
+      	
+            enabled: true,
+			sessionCookie: true
+        
+        },
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+		config : {
+			apiKey: "AIzaSyAxvPNNbc_ykgO2q_-Pg21eq3FOfbqRCCI",
+  
+			authDomain: "web3-shop-f55ec.firebaseapp.com",
+  
+			projectId: "web3-shop-f55ec",
+  
+			storageBucket: "web3-shop-f55ec.appspot.com",
+  
+			messagingSenderId: "265807226453",
+  
+			appId: "1:265807226453:web:6e045513314821294396dc",
+  
+			measurementId: "G-MKWT7MFTV7"
+
+		}
+  	},
+
     app:{
+    
         head:{
 
             script:[
@@ -15,10 +44,7 @@ export default defineNuxtConfig({
 
                 },
 
-
-
-                ],
-
+            ],
 
             link:[
 
@@ -34,8 +60,7 @@ export default defineNuxtConfig({
 
                 },
 
-
-                ]
+            ]
 
         }
     },
@@ -43,7 +68,3 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
 
 });
-
-
-
-
---
+++ pages/login.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+definePageMeta({
+  layout: 'front-layout'
+})
+
+import { signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+
+const auth = useFirebaseAuth()
+
+function loginWithGoogle(){
+
+	signInWithPopup(auth, new GoogleAuthProvider())
+
+}
+
+</script>
+<template>
+  
+	<div>
+    
+		<h1>登入</h1>
+    
+		<form @submit.prevent="login">
+      
+			<input v-model="email" type="email" placeholder="電子郵件" required />
+      
+			<input v-model="password" type="password" placeholder="密碼" required />
+      
+			<button type="submit">登入</button>
+    
+		</form>
+    
+		<button @click="loginWithGoogle">使用 Google 登入</button>
+    
+		<div v-if="error">{{ error }}</div>
+  
+	</div>
+
+</template>
+
+<style scoped>
+
+</style>
